ff05a2acfa040586697c63c4a4e36ad7b6b46da8
AlexSchaadhardt <81404292+AlexSchaadhardt@users.noreply.github.com>
2024-05-12 10:16:38 -0700

    Fix the delete buttons logic and clean styling of navBar.

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(auth(config));
 app.use('/uploads', express.static(path.join(filePath, 'uploads'))); // Serve static files from uploads directory
+app.use(express.static(path.join(__dirname,"public")));
 app.use('/user', userRouter);
 app.use('/post', postRouter);
 app.use('/search', searchRouter);
